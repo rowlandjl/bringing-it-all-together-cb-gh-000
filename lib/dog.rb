d6695f1ba449e35a5e09a5b28c57af1980cfcf91
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-02 18:51:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
-class Dog 
-  attr_accessor :name, :breed 
-  attr_reader :id 
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
 
   def initialize(name:, breed:, id: nil)
-    @id = id 
-    @name = name 
-    @breed = breed 
-  end 
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+end
